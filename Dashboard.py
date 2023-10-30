4c16203241834ae40d443aaf1c5de1b73f5d2553
RayhanLup1n <148042284+RayhanLup1n@users.noreply.github.com>
2023-10-30 21:12:54 +0700

    Update Dashboard.py

--- Dashboard.py
+++ Dashboard.py
@@ -3,6 +3,7 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 import streamlit as st
 import numpy as np
+from babel.numbers import format_currency
 sns.set(style='dark')
 
 days_df = pd.read_csv('days_df.csv')
