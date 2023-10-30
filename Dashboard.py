aee8b0001cebed14c92f9f7695f9a949dc5865c6
RayhanLup1n <148042284+RayhanLup1n@users.noreply.github.com>
2023-10-30 21:15:20 +0700

    Update Dashboard.py

--- Dashboard.py
+++ Dashboard.py
@@ -6,7 +6,7 @@ import numpy as np
 from babel.numbers import format_currency
 sns.set(style='dark')
 
-days_df = pd.read_csv('days_df.csv')
+days_df = pd.read_csv('https://github.com/RayhanLup1n/submissions/blob/main/days_df.csv')
 hours_df = pd.read_csv('hours_df.csv')
 
 def create_monthly_df(df):
